92ca6deada6d6ae59aa16a9b776c28f5fe60f694
Lukasz Woznicki <lukasz.woznicki@crowdstrike.com>
2026-02-03 16:36:49 +0000

    clippy and fmt on current

--- crates/rusty-falcon-experimental/src/client/mod.rs
+++
@@ -1,9 +0,0 @@
-mod cloud;
-mod credentials;
-mod error;
-mod falcon_client;
-
-pub use cloud::FalconCloud;
-pub use credentials::Credentials;
-pub use error::FalconError;
-pub use falcon_client::{FalconClient, FalconClientBuilder};
